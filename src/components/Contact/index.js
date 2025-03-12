1b4a0f762e6f319c9a474055c251be02c96d4956
Wan Qi Chen <96182313+ArielChen33@users.noreply.github.com>
2025-03-12 11:04:10 -0400

    Deleting DrawSVGPlugin for now

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -25,7 +25,7 @@ const Contact = () => {
                         idx={15}
                     />
                 </h1>
-                <p>
+                <p className="contact-method">
                     Email: wanqichen0924@gmail.com
                 </p>
                 
